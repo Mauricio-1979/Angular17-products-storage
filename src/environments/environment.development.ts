13ee96060e70210493d5715169aab9fb86eab3a1
mauricio <bomauricio9@gmail.com>
2024-03-18 12:21:19 -0300

    applying environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiEnd: 'http://localhost:4001/',
+  apiEnd: import.meta.env.NG_APP_API,
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiEnd: 'http://localhost:4001/'
+  //apiEnd: 'http://localhost:4001/'
+  apiEnd: import.meta.env.NG_APP_API
 };
